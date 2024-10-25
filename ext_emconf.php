0bb8b7f440edd866557fcd93f33107ac2bf81c02
Bill.大狗 <billdagou@gmail.com>
2024-10-25 16:27:30 +0800

    Upgrade to TYPO3 v12

--- ext_emconf.php
+++ ext_emconf.php
@@ -6,7 +6,7 @@ $EM_CONF[$_EXTKEY] = [
     'category' => 'plugin',
     'constraints' => [
         'depends' => [
-            'typo3' => '11.5.0-11.5.99',
+            'typo3' => '12.4.0-12.4.99',
         ],
     ],
     'state' => 'stable',
